0da6605224e1deeb8e03f24d9708ad1170c3c5f5
Team4239 <warpspeed@willmar.org>
2025-01-18 12:58:39 -0600

    mit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,5 +7,14 @@ public class Constants {
 
         PIVOT_MOTOR = 99,
 
-        REV_THROUGH_BORE_ENCODER = 16;
+        REV_THROUGH_BORE_ENCODER = 16,
+
+        BALL_INTAKE_MOTOR = 69,
+
+        CORAL_INTAKE_MOTOR = 59,
+
+        LIFT_MOTOR = 49,
+
+        EXTENTION_MOTOR = 29;
+    
 }
