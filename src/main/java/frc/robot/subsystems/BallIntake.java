58b70f20bc51affa11844023eaf3420d94d51613
Team4239 <warpspeed@willmar.org>
2025-01-18 13:53:31 -0600

    mit

---
+++ src/main/java/frc/robot/subsystems/BallIntake.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.robot.Constants;
+
+public class BallIntake extends SubsystemBase {
+
+  private final TalonFX mBallIntake = new TalonFX(Constants.BALL_INTAKE_MOTOR);
+
+  public BallIntake() {
+    mBallIntake.setInverted(false);
+    mBallIntake.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+  public void setSpeed(double speed) {
+    mBallIntake.set(speed);
+  }
+
+  public void stopMotor() {
+    mBallIntake.stopMotor();
+  }
+
+  @Override
+  public void periodic() {}
+
+}
---
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+private final TalonFX mLift = new TalonFX(Constants.LIFT_MOTOR_MOTOR);
+
+public class Lift extends SubsystemBase {
+  
+  public Lift() {}
+
+  @Override
+  public void periodic() {}
+}
