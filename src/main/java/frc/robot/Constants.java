8316c8b8d6b6599d366ebcf90196857fd1e01e20
Team4239 <warpspeed@willmar.org>
2025-01-18 13:39:31 -0600

    mit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+public class Constants {
+    public static final int 
+        XBOX_CONTROLLER = 0,
+        JOYSTICK = 1,
+
+        PIVOT_MOTOR = 99,
+
+        REV_THROUGH_BORE_ENCODER = 16,
+
+        BALL_INTAKE_MOTOR = 69,
+
+        CORAL_INTAKE_MOTOR = 59,
+
+        LIFT_MOTOR = 49,
+
+        EXTENTION_MOTOR = 29;
+    
+}
---
+++ src/main/java/frc/robot/subsystems/CoralIntake.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class CoralIntake extends SubsystemBase {
+
+  private final TalonFX mCoralIntake = new TalonFX(Constants.CORAL_INTAKE_MOTOR);
+  
+  public CoralIntake() {
+    mCoralIntake.setInverted(false);
+    mCoralIntake.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+  public void setSpeed(double speed) {
+    mCoralIntake.set(speed);
+  }
+
+  public void stopMotor() {
+    mCoralIntake.stopMotor();
+  }
+
+  public void setWheelSpeed(double topSpeed, double bottomSpeed) {
+    mCoralIntake.set(topSpeed);
+    mCoralIntake.set(bottomSpeed);
+  }
+  
+  @Override
+  public void periodic() {}
+}
