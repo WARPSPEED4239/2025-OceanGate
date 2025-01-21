ed8756ba0e9e2007fecf45f430bf67f5c79b0711
Team4239 <warpspeed@willmar.org>
2025-01-21 17:02:42 -0600

    test

--- src/main/java/frc/robot/subsystems/Lift.java
+++ src/main/java/frc/robot/subsystems/Lift.java
@@ -1,12 +1,24 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-private final TalonFX mLift = new TalonFX(Constants.LIFT_MOTOR_MOTOR);
+import frc.robot.Constants;
 
 public class Lift extends SubsystemBase {
+
+  private final TalonFX mLift = new TalonFX(Constants.LIFT_MOTOR);
   
-  public Lift() {}
+  public Lift() {
+    mLift.setInverted(false);
+    mLift.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+public void setSpeed(double speed) {
+  
+}
 
   @Override
   public void periodic() {}
