8924e575ce36adb78ff4cc60c326e31ac060880a
Team4239 <warpspeed@willmar.org>
2025-01-29 16:48:31 -0600

    Working Hard SMILEY

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,17 +12,25 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.commands.BIntake;
+import frc.robot.commands.CIntake;
 import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.BallIntake;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.subsystems.CoralIntake;
 
 public class RobotContainer {
     private double MaxSpeed = 0.5;//TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+
+    private final BallIntake mBallIntake = new BallIntake();
+    private final CoralIntake mCoralIntake = new CoralIntake();
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
@@ -34,10 +42,14 @@ public class RobotContainer {
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
     private final CommandXboxController joystick = new CommandXboxController(0);
+    private final CommandJoystick mJoystick = new CommandJoystick(Constants.JOYSTICK);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
     public RobotContainer() {
+
+        mBallIntake.setDefaultCommand(new BIntake(mBallIntake, 0.0));
+        mCoralIntake.setDefaultCommand(new CIntake(mCoralIntake, 0.0));
 
         UsbCamera mainCamera = CameraServer.startAutomaticCapture();
         mainCamera.setResolution(320, 240);
@@ -47,6 +59,13 @@ public class RobotContainer {
     }
 
     private void configureBindings() {
+
+        mJoystick.button(5).whileTrue(new BIntake(mBallIntake, 5));
+        mJoystick.button(3).whileTrue(new BIntake(mBallIntake, -5));
+        mJoystick.button(6).whileTrue(new CIntake(mCoralIntake, 5));
+        mJoystick.button(4).whileTrue(new CIntake(mCoralIntake, -5));
+
+
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
